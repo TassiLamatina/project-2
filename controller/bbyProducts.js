ee7844f40558f5f6c2e21ba74e136d0b346048da
Tlamatina <Tassi.lamatina@gmail.com>
2021-06-12 22:57:08 -0700

    routes done finally

--- controller/bbyProducts.js
+++ controller/bbyProducts.js
@@ -4,18 +4,56 @@ var router = express.Router();
 let axios = require('axios');
 
                 // ROUTES:
-router.get('/', async (req, res) => {
-    // TODO: Get all records from the DB and render to view
-    // let products = await db.pokemon.findAll();
-    // res.render('pokemon/index', { pokemons: pokemons });
-});
+// GET /products
+// router.get('/', async (req, res) => {
+//     // TODO: Get all records from the DB and render to view
+//     let products = await db.product.findAll();
+//     res.render('product/cart', { products: products });
+// });
 
 // GET ROUTE FOR /LOGIN PAGE displays log in form with boolean for matching username/password.
-
+// router.get('/login', (req, res) => {
+//     res.send('login.ejs');
+// });
 // POST ROUTE FOR /LOGING PAGE grabs user input and verifies boolean
+// router.post('/', async (req, res) => {
+//     // TODO: Get form data and add a new record to DB
+//     let product = await db.product.findOrCreate({
+//       where: {
+//         name: req.body.name
+//       }
+//     });
+//     res.redirect('/product');
+//   });
 
 // GET ROUTE FOR /PRODUCTS PAGE
 
-// POST ROUTE FOR /PRODUCTS PAGE
+// app.get('/products', function (req, res) {
+//     res.send('Hello World!')
+//   })
+
+// router.get('/product', (req, res) => {
+//     axios.get(`https://api.bestbuy.com/v1/products/8880044.json?apiKey=${BESTBUYAPI}`)
+//         .then((resFromAPI) =>{
+//             console.log(resFromAPI.data.Title)
+//             res.send(resFromAPI.data.Title)
+//         })
+//         .catch(err => {console.log(err)})
+// })
+
+// // POST ROUTE FOR /PRODUCTS PAGE
+// app.post('/products', function (req, res) {
+//     res.send('Got a POST request')
+//   })
+  
 
 // GET ROUTE FOR /PRODUCTS/:ID
+// router.get('/products/:id', (req, res) => {
+//     res.send('products.ejs');
+// });
+
+
+
+
+
+    module.exports = router;
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const layouts = require('express-ejs-layouts')
 let methodOverride = require('method-override');
 const router = express.Router();
 const controller = require('./controller/bbyProducts.js')
+const rowdy = require('rowdy-logger')
 
 
 // define API key
@@ -13,6 +14,7 @@ const BESTBUYAPI = process.env.BESTBUYAPI
 
 // declare apps
 const app = express()
+const rowdyResults = rowdy.begin(app)
 app.set('view engine', 'ejs')
 app.use(layouts) // use ejs layouts
 app.use('/', router);
@@ -25,10 +27,60 @@ app.get('/', (req, res) => {
   res.render('home.ejs')
 })
 
+// GET ROUTE FOR LOGIN PAGE
+app.get('/login', (req, res) => {
+  res.send('Hello World!')
+})
+
+// POST ROUTE FOR LOGIN PAGE
+app.post('/login', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+// GET ROUTE FOR /PRODUCTS PAGE
+app.get('/products', (req, res) => {
+  res.send('Hello World!')
+})
+
+// POST ROUTE FOR /PRODUCTS PAGE
+app.post('/products', (req, res) => {
+  res.send('Got a POST request')
+})
+
+// GET ROUTE FOR /PRODUCTS/:ID
+app.get('/products/:id', (req, res) => {
+  res.send('products.ejs');
+});
+
+//POST ROUTE FOR /PRODUCTS/:ID
+app.post('/products/:id', (req, res) => {
+  res.send('products.ejs');
+});
+
+// GET ROUTE FOR /CART
+app.get('/cart', function (req, res) {
+  res.send('Hello World!')
+})
+
+// DELETE ROUTE FOR /CART
+app.delete('/cart', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+// GET ROUTE FOR /ABOUTME
+app.get('/aboutme', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+
 // // Imports all routes from the controller routes file
-// app.use('/', require('./bbyProducts.js'));
+// app.use('/', require('..bbyProducts.js'));
 
 //open up port for the app to listen on + define port
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening to the smooth sweet sounds of K${process.env.PORT} in the morning ☕️.`)
+ rowdyResults.print()
 });
