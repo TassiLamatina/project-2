f80a575e53f8de085975bb4c138535dab93df380
Tlamatina <Tassi.lamatina@gmail.com>
2021-06-15 13:24:09 -0700

    almost mvp

--- index.js
+++ index.js
@@ -28,31 +28,46 @@ app.get('/', (req, res) => {
   res.render('home.ejs')
 })
 
+// POST ROUTE FOR LOGIN PAGE
+app.post('/login', (req, res) => {
+  let currentUser = req.body.userName
+  
+  res.render('login.ejs', { userName: currentUser })
+})
+
 // GET ROUTE FOR LOGIN PAGE
 app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
 
-// POST ROUTE FOR LOGIN PAGE
-app.post('/login', (req, res) => {
-  res.render('login.ejs')
-})
+
 
 
 // GET ROUTE FOR /PRODUCTS PAGE
 app.get('/products', (req, res) => {
+let currentUser = req.query.user
+console.log(req.query)
+db.user.findOne({
+  where: {
+    userName: currentUser
+  } 
+}) .then(user => {
+
+
   axios.get(`https://api.bestbuy.com/v1/products(search=${ req.query.name })?format=json&show=name,thumbnailImage,regularPrice,salePrice,description,customerTopRated,mediumImage,addToCartUrl,longDescription,color,condition,largeImage,modelNumber,percentSavings&apiKey=${BESTBUYAPI}`)
-        .then((resFromAPI) =>{ console.log(req.query.name)
-            console.log(resFromAPI.data)
+        .then((resFromAPI) =>{ 
+        // console.log(req.query.name)
+        //     console.log(resFromAPI.data)
+        console.log(user,"😂😂😂😂😂😂")
           const products = resFromAPI.data.products.filter( (product) =>{
             const savings = parseFloat(product.percentSavings)
             return savings > 40
           })
-            res.render('./products/results.ejs', {products: products})
+            res.render('./products/results.ejs', {products: products, userName:user.userName},)
 
         })
         .catch(err => {console.log(err)})
-  
+      })
 });
  
 
@@ -61,22 +76,40 @@ app.get('/results', (req, res) => {
   res.render('./products/results.ejs');
 });
 
-//POST ROUTE FOR /PRODUCTS/:ID
-// app.post('/products', (req, res) => {
-//   // e.preventDefault()
-//   let searchQuery = req.query.name
-// })
 
 // GET ROUTE FOR /CART
 app.get('/cart', function (req, res) {
-  res.render('cart.ejs')
+  res.render('cart')
+})
+
+// post cart
+app.post('/cart', function (req, res) {
+  db.product.findOrCreate({
+    where: { product: product.name },
+    defaults: {
+        name: product.name,
+        thumbnailImage: product.thumbnailImage,
+        regularPrice: product.regularPrice,
+        salePrice: product.salePrice,
+        description: product.description,
+        customerTopRated: product.customerTopRated,
+        mediumImage: product.mediumImage,
+        addToCartUrl: product.addToCartUrl,
+        longDescription: product.longDescription,
+        color: product.color,
+        condition: product.condition,
+        largeImage: product.largeImage,
+        modelNumber: product.modelNumber,
+        percentSavings: product.percentSavings
+    }
+})
+  res.redirect('/cart')
 })
 
 // DELETE ROUTE FOR /CART
-app.delete('/cart', function (req, res) {
-  res.render('cart.ejs')
-})
-
+// app.delete('/cart', function (req, res) {
+//   res.render('cart.ejs')
+// })
 
 // GET ROUTE FOR /ABOUTME
 app.get('/about', function (req, res) {
@@ -106,8 +139,10 @@ app.post('/join', function (req, res) {
         userName: req.body.userName,
         password: req.body.password
     }
+}) .then ( ([user, created])=> {
+  res.redirect(`/products?user=${user.userName}`)
 })
-  res.redirect('/products')
+  
 })
 
 
